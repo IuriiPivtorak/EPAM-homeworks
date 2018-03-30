42ac7e48e0f65bb3566c72572552ab6a659101fe
IuriiPivtorak <pivtorakuv@yandex.ru>
2018-03-30 13:21:06 +0300

    redone

--- homework-2/intersection_and_union.py
+++ homework-2/intersection_and_union.py
@@ -3,12 +3,18 @@ def intersect(*args):
     sets = []
     try:
         for i in args:
-            new_set = set(i)
+            new_set = i
             sets.append(new_set)
         int_set = sets[0]
         for j in range(len(sets) - 1):
-            int_set = int_set & sets[j + 1]
-        return list(int_set)
+            result = []
+            for i in int_set:
+                if i in sets[j + 1]:
+                    result.append(i)
+                    int_set = result[:]
+                else:
+                    pass
+        return list(result)
     except UnboundLocalError:
         print('use at least 2 sets')
 
@@ -19,22 +25,31 @@ s2 = (1, 3, 4, 5)
 s3 = (1, 3, 5, 6)
 s4 = ['A', 'B']
 s5 = ['A', 'C']
-print(intersect(s1,s2))
-print(intersect(s1,s3))
-print(intersect(s1,s2,s3))
-print(intersect(s4,s5))
-print(intersect(s1,s2,s3,s4,s5))
+print(intersect(s1, s2))
+print(intersect(s1, s3))
+print(intersect(s1, s2, s3))
+print(intersect(s4, s5))
+print(intersect(s1, s2, s3, s4, s5))
 
 # union function
 def union(*args):
     sets = []
-    for i in args:
-        new_set = set(i)
-        sets.append(new_set)
-    union_set = sets[0]
-    for j in range(len(sets) - 1):
-        union_set = union_set | sets[j + 1]
-    return list(union_set)
+    try:
+        for i in args:
+            new_set = i
+            sets.append(new_set)
+        int_set = sets[0]
+        for j in range(len(sets) - 1):
+            result = []
+            for i in int_set:
+                for k in sets[j + 1]:
+                    result.append(i)
+                    result.append(k)
+                    result = list(set(result))
+                    int_set = result[:]
+        return list(result)
+    except UnboundLocalError:
+        print('use at least 2 sets')
 
 
 # testing
