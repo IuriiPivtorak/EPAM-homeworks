abc75005b9a3795939f91363ed7b37c97fee4a8c
IuriiPivtorak <pivtorakuv@yandex.ru>
2018-04-01 15:08:03 +0300

    fix

--- homework-2/intersection_and_union.py
+++ homework-2/intersection_and_union.py
@@ -1,17 +1,17 @@
 # intersection function
-def intersect(*args):
+def intersect(*data):
     """ This function makes
     intersections between sets.
 
-    :param args: sets to use.
-    :type args: str, list, tuple, set.
+    :param data: sets to use.
+    :type data: str, list, tuple, set.
     :returns: set of intersected elements.
     :raises: 'use at least 2 sets'
     """
     # preparing the list to store sets of elements.
     sets = []
     try:
-        for i in args:
+        for i in data:
             new_set = list(i)
             sets.append(new_set)
         int_set = sets[0]
@@ -27,25 +27,25 @@ def intersect(*args):
                     int_set = result[:]
                 else:
                     pass
-        return set(result)
+        return list(result)
     except UnboundLocalError:
         print('use at least 2 sets')
 
 
 # union function
-def union(*args):
+def union(*data):
     """ This function makes
     unions between sets.
 
-    :param args: sets to use.
-    :type args: str, list, tuple, set.
+    :param data: sets to use.
+    :type data: str, list, tuple, set.
     :returns: set of unified elements.
     :raises: 'use at least 2 sets'
     """
     # preparing the list to store sets of elements.
     sets = []
     try:
-        for i in args:
+        for i in data:
             new_set = i
             sets.append(list(new_set))
         int_set_1 = sets[0]
